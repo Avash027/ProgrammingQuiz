98e01fee117b29c8578cb6e0589b6b611a645809
unknown <avashmitra2706@gmail.com>
2021-01-07 11:30:18 +0530

    Added documentation to App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
-import Appbar from "./Styling/Appbar";
+import Appbar from "./Styling/Appbar"; //Using Material-UI here
 import { Button, TextField } from "@material-ui/core";
 
-import Questions from "./QuestionGeneration/Questions";
+import Questions from "./QuestionGeneration/Questions"; //This is for presenting the questions
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      questions: [],
-      correct_ans: [],
-      ans: [],
+      questions: [], //Stores all the questions
+      correct_ans: [], //Stores all the correct answers
+      ans: [], //Stores all the options . It is a 2-D array
       hasGeneratedQuiz: false,
       score: 0,
       isAnswered: [],
-      topic: "18",
+      topic: "18", //Programming questions are defined as topic 18 in database
       difficulty: "easy",
       value: "10",
-      hasErrorOccured: 0,
+      hasErrorOccured: 0, //Incase
     };
   }
 
@@ -26,6 +26,7 @@ class App extends Component {
 
     const noOfQues = parseInt(this.state.value);
 
+    //For validating user input
     if (noOfQues <= 0 || noOfQues > 50 || !Number.isInteger(noOfQues)) {
       alert("Enter Valid No. of Questions");
       return;
@@ -35,7 +36,7 @@ class App extends Component {
       `https://opentdb.com/api.php?amount=${this.state.value}&category=${this.state.topic}&type=multiple`
     );
     let data = await response.json();
-
+    //If data could not be fetched we print the error
     if (data.response_code !== 0) {
       this.setState({
         hasErrorOccured: 1,
@@ -51,11 +52,11 @@ class App extends Component {
     let isAnswered = [];
 
     for (var i = 0; i < results.length; i++) {
-      isAnswered.push("no");
+      isAnswered.push("no"); //Initially none of the questions are answered
       questions.push(results[i].question);
       correct_ans.push(results[i].correct_answer);
       var temp = [results[i].correct_answer, ...results[i].incorrect_answers];
-      shuffleArray(temp);
+      shuffleArray(temp); //This is to shuffle the array
       ans.push(temp);
     }
 
@@ -73,22 +74,28 @@ class App extends Component {
     event.preventDefault();
 
     let isAns = [...this.state.isAnswered];
+    //The class name shows the option selected by the user
+    //And id shows the index of correct answer
     if (event.target.className === this.state.correct_ans[event.target.id]) {
+      //If the answer is correct
       isAns[event.target.id] = "right";
       this.setState({
         score: this.state.score + 1,
         isAnswered: isAns,
       });
     } else {
+      //If the answer is wrong
       isAns[event.target.id] = "wrong";
       this.setState({
         isAnswered: isAns,
       });
     }
 
+    //After the user enters the answer we check if all the questions are answered
     const allAnswered = isAns.every((val) => {
       return val === "right" || val === "wrong";
     });
+    //If all answered we scroll to top to show the score to user
     if (allAnswered) {
       document.body.scrollTop = 0;
       document.documentElement.scrollTop = 0;
@@ -103,23 +110,23 @@ class App extends Component {
   };
 
   render() {
-    let elem = "";
+    let content = "";
 
     if (this.state.hasErrorOccured) {
-      elem = (
+      content = (
         <center>
           <h1>Something Went Wrong !!!</h1>
         </center>
       );
     } else if (this.state.hasGeneratedQuiz) {
-      elem = (
+      content = (
         <Questions
           data={this.state}
           click={(event) => this.submitAnswer(event)}
         ></Questions>
       );
     } else {
-      elem = "";
+      content = "";
     }
 
     return (
@@ -149,11 +156,17 @@ class App extends Component {
           </Button>
         </center>
         <br></br>
-        {elem}
+        {content}
       </div>
     );
   }
 }
+
+/**
+ *
+ * @param {An array of strings} array
+ *
+ */
 
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
